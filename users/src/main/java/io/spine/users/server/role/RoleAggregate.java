5dc18f57361144ca1117bece962981f1e3eb479f
Dmytro Dashenkov <dmytro.dashenkov@teamdev.com>
2019-03-07 11:33:30 +0200

    Remove <h1> tag from Javadoc.

--- users/src/main/java/io/spine/users/server/role/RoleAggregate.java
+++ users/src/main/java/io/spine/users/server/role/RoleAggregate.java
@@ -46,11 +46,9 @@ import io.spine.users.role.event.RoleDeletedVBuilder;
  * only to those users and groups that are in the same organization and/or orgunit
  * (or their child orgunits).
  *
- * <h1>Access Control</h1>
- *
- * The roles assigned to a {@linkplain io.spine.users.server.group.GroupPart group} are recursively
- * propagated to all members of the group. This propagation is implicit and is not reflected in
- * aggregate states.
+ * <p><b>Access Control.</b> The roles assigned to
+ * a {@linkplain io.spine.users.server.group.GroupPart group} are recursively propagated to all
+ * members of the group. This propagation is implicit and is not reflected in aggregate states.
  *
  * <p>Therefore, when carrying out role-based access control, consider that a
  * {@link io.spine.users.server.user.UserPart User} and {@link io.spine.users.server.group.GroupPart
