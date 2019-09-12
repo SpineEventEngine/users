5e9cfade5657b8dc3c27318fd7143aa850e58c22
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2019-09-12 17:47:04 +0300

    Fix environment for sign-out test

--- users/src/test/java/io/spine/users/server/signin/SignUserOutCommandTest.java
+++ users/src/test/java/io/spine/users/server/signin/SignUserOutCommandTest.java
@@ -21,31 +21,57 @@
 package io.spine.users.server.signin;
 
 import io.spine.core.UserId;
+import io.spine.testing.server.blackbox.MultitenantBlackBoxContext;
+import io.spine.users.server.Directory;
+import io.spine.users.server.signin.given.StubDirectory;
 import io.spine.users.signin.command.SignUserIn;
 import io.spine.users.signin.command.SignUserOut;
 import io.spine.users.signin.event.SignOutCompleted;
+import io.spine.users.user.Identity;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static io.spine.users.server.signin.Signals.signIn;
 import static io.spine.users.server.signin.given.SignInTestCommands.signOutCommand;
 import static io.spine.users.server.signin.given.SignInTestEnv.identity;
 import static io.spine.users.server.signin.given.SignInTestEnv.userId;
+import static io.spine.users.server.user.given.UserTestEnv.profile;
 
 @DisplayName("`SignInPm` should, when `SignUserOut` command is dispatched to it,")
 class SignUserOutCommandTest extends SignInProcessTest {
 
+    private UserId userId;
+
+    @Override
+    protected @Nullable Directory createDirectory() {
+        Identity identity = identity();
+        return new StubDirectory()
+                .addIdentity(identity)
+                .allowSignInFor(identity)
+                .addProfile(identity, profile());
+    }
+
+    @BeforeEach
+    void signTheUserIn() {
+        userId = userId();
+        SignUserIn signIn = signIn(userId, identity());
+        context().receivesCommand(signIn);
+    }
+
     @Test
     @DisplayName("emit `SignOutCompleted` event")
     void emitSignOutCompleted() {
-        UserId userId = userId();
-        SignUserIn signIn = Signals.signIn(userId, identity());
         SignUserOut signOut = signOutCommand(userId);
-        SignOutCompleted expectedEvent =
-                SignOutCompleted
-                        .newBuilder()
-                        .setId(userId)
-                        .build();
-        context().receivesCommands(signIn, signOut)
+        MultitenantBlackBoxContext afterCommand = context().receivesCommand(signOut);
+
+        SignOutCompleted expectedEvent = SignOutCompleted
+                .newBuilder()
+                .setId(userId)
+                .build();
+
+        afterCommand
                  .assertEvents()
                  .withType(expectedEvent.getClass())
                  .message(0)
